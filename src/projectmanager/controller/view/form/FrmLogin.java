e8f4693a538e0fa3ec35dc2211d2df31a944d149
emapiric <emapiric998@gmail.com>
2021-01-26 20:05:34 +0100

    deleted uneccessary methods

--- src/projectmanager/controller/view/form/FrmLogin.java
+++ src/projectmanager/controller/view/form/FrmLogin.java
@@ -42,7 +42,6 @@ public class FrmLogin extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         inputUsername = new projectmanager.controller.view.component.InputTextFieldPanel();
         btnLogin = new javax.swing.JButton();
-        btnSignup = new javax.swing.JButton();
         inputPassword = new projectmanager.controller.view.component.InputPasswordPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -53,8 +52,6 @@ public class FrmLogin extends javax.swing.JFrame {
         jLabel1.setText("Project Manager");
 
         btnLogin.setText("Login");
-
-        btnSignup.setText("Sign up");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -67,9 +64,7 @@ public class FrmLogin extends javax.swing.JFrame {
                         .addComponent(jLabel1))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(192, 192, 192)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(btnSignup, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(93, 93, 93)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -88,9 +83,7 @@ public class FrmLogin extends javax.swing.JFrame {
                 .addComponent(inputPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(56, 56, 56)
                 .addComponent(btnLogin)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnSignup)
-                .addContainerGap(23, Short.MAX_VALUE))
+                .addContainerGap(53, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -146,7 +139,6 @@ public class FrmLogin extends javax.swing.JFrame {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnLogin;
-    private javax.swing.JButton btnSignup;
     private projectmanager.controller.view.component.InputPasswordPanel inputPassword;
     private projectmanager.controller.view.component.InputTextFieldPanel inputUsername;
     private javax.swing.JLabel jLabel1;
